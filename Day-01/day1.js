8077edbed8cab9ddcb6d5cf19762b12e9e119531
Om Deshmukh <omdeshmukh4455@gmail.com>
2026-01-16 21:53:29 +0530

    :Day 1: JS Basics , Varaibles and Datatypes

---
+++ Day-01/day1.js
@@ -0,0 +1,48 @@
+console.log("Hello JavaScript");
+console.log("Day 1 started");
+
+let myName = "Om";
+let myAge = 20;
+let isStudent = true;
+
+
+console.log(typeof myName);     // string
+console.log(typeof myAge);      // number
+console.log(typeof isStudent);  // boolean
+
+
+let x;
+let y = null;
+
+console.log(typeof x);
+console.log(typeof y);
+
+
+let score = 10;
+score = 20;
+
+console.log(score);
+
+/*
+const pi = 3.14;
+pi = 3.1415; // ERROR
+
+*/
+
+//Task 1 
+
+let name = "Jayu Deshmukh"
+let college = "VIT"
+let branch = "ENTC"
+let goal = "Become a skilled JS Developer"
+
+
+console.log(name);
+console.log(college);
+console.log(branch);
+console.log(goal);
+
+
+
+
+
